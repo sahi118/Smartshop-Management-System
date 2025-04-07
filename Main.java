21a474901aa76653b6da6b585f2c584203eb7fa2
sahi118 <sahilakhtar995@icloud.com>
2025-04-07 14:34:34 +0100

    Main Menu code

--- Main.java
+++ Main.java
@@ -1,10 +1,33 @@
+package smartshopManagement;
+import java.awt.GridLayout;
 import javax.swing.*;
 
-package smartshopManagement;
-
 public class Main {
-    public static void main(String[] args) {
 
+    public Main(){
+        JFrame menu = new JFrame();
+		
+		JButton btn1 = new JButton("Manage Inventory");
+		JButton btn2 = new JButton("Record Sales");
+		JButton btn3 = new JButton("Generate Report");
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(3,1,50,50));
+		panel.add(btn1);
+		panel.add(btn2);
+		panel.add(btn3);
+		
+		menu.add(panel);
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu.setTitle("Smart Shop Management System");
+		menu.setSize(300,550);
+		menu.setVisible(true);
+		
+	}
+
+    
+    public static void main(String[] args) {
+        new Main();
     }
 }
 
