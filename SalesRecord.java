45bb4d4c5d29629efba63b440780464d5aaa5999
SqueamishEel <dzaccy@outlook.com>
2025-04-25 17:37:37 +0100

    Basic Report Generation

--- SalesRecord.java
+++ SalesRecord.java
@@ -1,5 +1,21 @@
 package smartshopManagement;
+import java.io.FileWriter;
 
-public class SalesRecord {
-
+public class SalesRecord
+{
+    public static void fileGeneration(String[] args)
+    {
+        try 
+        {
+            FileWriter myWriter = new FileWriter("New Report.txt");
+            myWriter.write("New Report:");
+            myWriter.write("");
+            myWriter.write("");
+            myWriter.close();
+        } 
+        catch (Exception e)
+        {
+        }
+        
+    } 
 }
