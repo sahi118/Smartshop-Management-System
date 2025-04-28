bd5d8d5c1ad632aca545d9dc80e7d55dd160a83d
SqueamishEel <dzaccy@outlook.com>
2025-04-28 21:16:28 +0100

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,17 +1,88 @@
 package smartshopManagement;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Product {
-    String name;
-    String price;
-    String quantity;
+public class Product extends JPanel
+{
+    //Product List
+    public static String ProductName;
+    public static String ProductPrice;
+    public static String ProductQuantity;
 
-    public Product(String name, String price, String quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-    @Override
-    public String toString() {
-        return name + " | £" + ( price) + " | Qty: " + quantity;
+    //Sales Page
+    public String TotalSales;
+    public JButton AddProduct;
+    public JButton AddSales;
+
+    //Report
+    public String CompletedString;
+
+    Product()
+    {
+        this.setPreferredSize(new Dimension(400,40));
+        this.setBackground(Color.decode("#E0F7FA"));
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        this.setLayout(new FlowLayout());
+
+		JLabel productNameTitle = new JLabel("Name:");
+        JLabel ProductName = new JLabel();
+		JLabel productPriceTitle = new JLabel("£");
+        JLabel ProductPrice = new JLabel();
+		JLabel productQuantityTitle = new JLabel("Amt:");
+        JLabel ProductQuantity = new JLabel();
+        JLabel TotalSalesTitle = new JLabel("Total Sales:");
+        JLabel TotalSales = new JLabel("0");
+
+        JButton AddProduct = new JButton("Increase Inventory");
+		AddProduct.setFont(new Font("Tahoma",Font.BOLD, 10));
+		AddProduct.setForeground(Color.decode("#E0F7FA"));
+		AddProduct.setBackground(Color.decode("#004D40"));
+		AddProduct.setFocusPainted(false);
+		AddProduct.setBorderPainted(true);
+		AddProduct.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                AddProduct.setBackground(Color.decode("#00796B"));
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                AddProduct.setBackground(Color.decode("#004D40")); 
+            }
+        });
+        AddProduct.addActionListener((ActionEvent e) -> {
+            //increase inventory
+        });
+
+        JButton AddSales = new JButton("Add Sale");
+		AddSales.setFont(new Font("Tahoma",Font.BOLD, 10));
+		AddSales.setForeground(Color.decode("#E0F7FA"));
+		AddSales.setBackground(Color.decode("#004D40"));
+		AddSales.setFocusPainted(false);
+		AddSales.setBorderPainted(true);
+		AddSales.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                AddSales.setBackground(Color.decode("#00796B"));
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                AddSales.setBackground(Color.decode("#004D40")); 
+            }
+        });
+        AddSales.addActionListener((ActionEvent e) -> {
+            //add sales
+        });
+
+        this.add(productNameTitle);
+        this.add(ProductName);
+        this.add(productPriceTitle);
+        this.add(ProductPrice);
+        this.add(productQuantityTitle);
+        this.add(ProductQuantity);
+        this.add(TotalSalesTitle);
+        this.add(TotalSales);
+        this.add(AddProduct);
+        this.add(AddSales);
     }
 }
