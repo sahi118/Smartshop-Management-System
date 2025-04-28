f32f35a4ad34f3ef29da769c7d842611fea40f5c
SqueamishEel <dzaccy@outlook.com>
2025-04-28 21:18:19 +0100

    Update InventoryManagement.java

--- InventoryManagement.java
+++ InventoryManagement.java
@@ -1,7 +1,5 @@
 package smartshopManagement;
 import java.util.ArrayList;
-import javax.swing.*;
-import javax.swing.table.*;
 
 public class InventoryManagement {
     private ArrayList<Product> stock;
@@ -11,12 +9,6 @@ public class InventoryManagement {
 
     public String printStock() {
         StringBuilder sb = new StringBuilder();
-
-        String[]columns  = {"Name","Price","Quantity"};
-        DefaultTableModel model = new DefaultTableModel(columns,0);
-        JTable table = new JTable(model);
-        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        JScrollPane tabScrollPane = new JScrollPane(table);
 
         for (Product p : stock) {
             sb.append(p.toString()).append("\n");
