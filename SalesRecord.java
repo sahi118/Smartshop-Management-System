1adc4bb1f76442902218586ed562041b96dcecb0
SqueamishEel <dzaccy@outlook.com>
2025-04-30 12:15:20 +0100

    Update SalesRecord.java

--- SalesRecord.java
+++ SalesRecord.java
@@ -1,26 +1,34 @@
-package smartshopManagement;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.*;
 
 public class SalesRecord
 {
-    public static void fileGeneration(String[] args)
-    {
-        JOptionPane.showMessageDialog(null,"Report has been generated");
-        
-        try 
-        {
-            FileWriter myWriter = new FileWriter("New Report.txt");
-            myWriter.write("New Report:");
-            myWriter.write("");
-            myWriter.write("");
-            myWriter.close();
-        } 
-        catch (IOException e)
-        {
-            
-        }
-        
+    public static void fileGeneration(java.util.List<String> reportEntries)
+    {     
+        try {
+            File file = new File("Report.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+                JOptionPane.showMessageDialog(null, file.getName() + " has been created");
+            }
+            FileWriter writer = new FileWriter(file, false);
+            writer.write("Sales Record\n");
+            for (String entry : reportEntries) {
+                writer.write(entry + "\n");
+            }
+            writer.write("-------------------\n");
+            writer.close();
+
+
+            JOptionPane.showMessageDialog(null, file.getName() + " Report added successfully");
+
+        }    catch(IOException e)
+            {
+                System.out.println("error occurred.");
+                e.printStackTrace();
+            }
+
     } 
 }
