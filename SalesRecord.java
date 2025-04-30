36503c678760829046eaf43739399ced641a492d
SqueamishEel <dzaccy@outlook.com>
2025-04-30 21:09:38 +0100

    Added Comments

--- SalesRecord.java
+++ SalesRecord.java
@@ -1,3 +1,4 @@
+//Importing Required Features
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,11 +9,14 @@ public class SalesRecord
     public static void fileGeneration(java.util.List<String> reportEntries)
     {     
         try {
+            //Creates new file or replaces currently existing file
             File file = new File("Report.txt");
             if (!file.exists()) {
                 file.createNewFile();
+                //Displays pop-up telling user file has been created
                 JOptionPane.showMessageDialog(null, file.getName() + " has been created");
             }
+            //Writes the current content of the arraylist in Main to the txt file
             FileWriter writer = new FileWriter(file, false);
             writer.write("Sales Record\n");
             for (String entry : reportEntries) {
@@ -21,11 +25,12 @@ public class SalesRecord
             writer.write("-------------------\n");
             writer.close();
 
-
+        //Displays pop-up telling user file has been added
             JOptionPane.showMessageDialog(null, file.getName() + " Report added successfully");
 
         }    catch(IOException e)
             {
+                //Displays pop-up telling user an error has occured
                 System.out.println("error occurred.");
                 e.printStackTrace();
             }
